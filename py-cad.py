857c9c4fb3aa19043e9a4f1917219b88d0035f7f
第一次 <xieyuleiya@qq.com>
2024-05-09 03:51:02 +0800

    第四次

---
+++ py-cad.py
@@ -0,0 +1,17 @@
+"""
+by leilei
+备注：
+"""
+
+import win32com.client
+
+# 连接到AutoCAD应用程序
+cad = win32com.client.Dispatch("AutoCAD.Application")
+
+doc = cad.ActiveDocument
+
+doc.Utility.Prompt("hello")
+
+print(doc.Name)
+
+
